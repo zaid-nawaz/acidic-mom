150a276659721feb34faf6d8b671dd57fc31cff4
zaid-nawaz <zaidnawaz2005@gmail.com>
2026-03-19 13:02:13 +0530

    fixed bugs

--- src/components/course-link.tsx
+++ src/components/course-link.tsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 export const CourseLink = ({ courseId, itemCount, videoId, createEntry } : any) => {
     return (
         <div>
-        <Link href={`/courses/${courseId}/${itemCount}/${videoId}`} onClick={createEntry}>Go to course</Link>
+        <a href={`/courses/${courseId}/${itemCount}/${videoId}`} onClick={createEntry}>Go to course</a>
         </div>
     )
 }
